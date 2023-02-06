c13f883497158fba22b830ba79413387067ff9fd
EggadiSrikanth <124619746+EggadiSrikanth@users.noreply.github.com>
2023-02-06 23:45:12 +0530

    Add files via upload

---
+++ Conditional_Statment.java
@@ -0,0 +1,56 @@
+
+public class Conditional_Statment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int Srikanth=30;
+//		int Saipallavi=20;
+//w.a.p To  Check weather S and S are Eligible for mrg or not.
+//		if((Srikanth>=25)&&(Saipallavi>=18)) {
+//		System.out.println("Eligible For Mrg");
+//		}else {
+//			
+//		System.out.println("Not Eligible For Mrg");
+//		}
+//w.a.p To Check weather the given single char contain the digit or not
+//		char S='4';
+//		if ((S>='0')&&(S<='9')) {
+//		System.out.println("single char contain the digit");	
+//		}else {
+//			System.out.println("single char doesn't contain the digit");
+//		}
+		
+//w.a.p To Check weather the given single char contain lower case Alphabet
+//		char Ch='A';
+//		if(Ch>='A'&& Ch<='Z') {
+//			System.out.println("contain the Uppercase");
+//		}else {
+//			System.out.println("contain the Lowercase");	
+//		}
+//w.a.p To Check weather the given single char contain An Alphabet
+//		char Ch='1';
+//		if((Ch>='a'&& Ch<='z')||(Ch>='A'&& Ch<='z')) {
+//			System.out.println('S'+" contain the Alphabet");
+//		}else {
+//			System.out.println('S'+" doesn't contain the Alphabet");	
+//		}
+		
+//W.A P like right angle triangle with a number which will repeat a number in a row.
+//	   int i,j;
+//	   for(i=1; i<=4; i++) {
+//		   for(j=1; j<=i;j++)
+//		   System.out.print(i);
+//	       System.out.println("");
+//		  
+//	   }
+//print such pattern like right angle triangle with number increased by 1.The pattern like :
+//		int i,j,k=1;
+//		for (i=1; i<=10;i++) {
+//			for(j=1;j<=i;j++)
+//			System.out.print(i++);
+//			System.out.println("");
+//		}
+		
+	}
+
+}
