997b9805a27b1ed059dd22837dca32d45b64a7a0
EggadiSrikanth <124619746+EggadiSrikanth@users.noreply.github.com>
2023-02-07 23:52:06 +0530

    Add files via upload

---
+++ Logical_Operator.java
@@ -0,0 +1,37 @@
+package logical_operator;
+
+public class Logical_Operator {
+
+	public static void main(String[] args) {
+		// w.a.p using logical operator 
+//		int S = 14;
+//		if ((S % 3 == 0) && (S % 4 == 0)) {
+//			System.out.println("Gm");
+//
+//		} else
+//
+//		{
+//			System.out.println("Gn");
+//		}
+		
+		// w.a.p wether the couples are eligible for mrg using && Operator.
+//		int srikanth=24;
+//		int siri=16;
+//		
+//	String lucky=(siri>18)&&(srikanth>22)?"Both are Elgible":"Both are Not Eligible";
+//	
+//	System.out.println(lucky);
+		
+// w.a.p wether the couples are eligible for mrg using || Operator	
+		int srikanth=24;
+		int siri=16;
+		
+	String lucky=(siri>18)||(srikanth>22)?"Both are Elgible":"Both are Not Eligible";
+	
+	System.out.println(lucky);
+		
+		
+		
+	}
+
+}
