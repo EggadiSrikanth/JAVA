d0e1bdc5fca0a1ed66ea8bf8bcdc32e96cde3ff4
EggadiSrikanth <124619746+EggadiSrikanth@users.noreply.github.com>
2023-02-07 23:51:44 +0530

    Add files via upload

---
+++ conditional_operator.java
@@ -0,0 +1,15 @@
+package logical_operator;
+
+public class conditional_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 100;
+		if (num % 2 == 0) {
+			System.out.println("Number is even");
+		} else {
+			System.out.println("Number is odd");
+		}
+	}
+
+}
